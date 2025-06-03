b04c86ca15e5d0571cc0f14ea03149ebde5af142
adam <kaw94@hotmail.co.uk>
2025-06-03 10:23:44 +0100

    MOVED APPKIT APIKEY

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDOAFPA3pvHUfkB75kkk3mwi17pfMw83ZA",
-  authDomain: "pulseboard-75e35.firebaseapp.com",
-  projectId: "pulseboard-75e35",
-  storageBucket: "pulseboard-75e35.firebasestorage.app",
-  messagingSenderId: "776421741142",
-  appId: "1:776421741142:web:9a6668e6ad9a6a713de1f7",
-  measurementId: "G-LHV9H38BNK",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase safely
